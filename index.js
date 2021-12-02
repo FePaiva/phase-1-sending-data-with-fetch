1521f1c590e22eb585c6b2e201f530b664055483
Felippe Alves de Paiva <felippeapaiva@gmail.com>
2021-12-01 20:03:43 -0800

    commit tree 62179a2b3617f53b90acc27342e553740e7d4959

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 // Add your code here
+
+const userURL = "http://localhost:3000/users"
+
+let userData = {
+  name: "Steve",
+  email: "steve@steve.com"
+}
+
+let objPost = {
+  method: "POST",
+  headers: {
+    "Content-type": 'application/json',
+    "Accept": 'application/json' 
+},
+body: JSON.stringify(userData)
+}
+
+function submitData (){
+  return fetch(userURL, objPost)
+  .then(response => response.json())
+  .then(getId)
+  .catch(errorFunc)
+}
+
+const bodyTag = document.querySelector('body')
+
+function getId(object){
+  // document.body.innerHTML = object.id
+  const li = document.createElement('li')
+  li.innerHTML = object.id
+  bodyTag.appendChild(li)
+}
+
+function errorFunc(error){
+  // document.body.innerHTML = error.message
+  const newLi = document.createElement('li')
+  newLi.innerHTML = error.message
+  bodyTag.appendChild(newLi)
+}
